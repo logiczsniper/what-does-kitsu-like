e229102b03ede1bca6259150cf191a8bc63682ab
logiczsniper <lczernel@gmail.com>
2023-03-02 19:31:21 +0000

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/what-does-kitsu-like/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/what-does-kitsu-like/" : "/",
+  publicPath: "",
 };
